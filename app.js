9b44b36c8498c5a9bd0b72c309b82fd8d44b6b20
marioclq <mccalloluque@gmail.com>
2024-06-28 12:03:40 -0500

    add icons

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("soundCanvas");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 const frequencySlider = document.getElementById("frequency");
@@ -9,6 +11,7 @@ const playPauseButton = document.getElementById("playPause");
 const resetButton = document.getElementById("reset");
 const moveSourceButton = document.getElementById("moveSource");
 const soundToggle = document.getElementById("soundToggle");
+
 
 let frequency = frequencySlider.value;
 let amplitude = amplitudeSlider.value;
@@ -47,158 +50,169 @@ oscillator.start();
 audioCtx.suspend();
 
 frequencySlider.addEventListener("input", (e) => {
-  frequency = e.target.value;
-  frequencyValue.textContent = frequency;
-  oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+    frequency = e.target.value;
+    frequencyValue.textContent = frequency;
+    oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime);
 });
 
 amplitudeSlider.addEventListener("input", (e) => {
-  amplitude = e.target.value;
-  amplitudeValue.textContent = amplitude;
-  gainNode.gain.setValueAtTime(amplitude / 100, audioCtx.currentTime);
+    amplitude = e.target.value;
+    amplitudeValue.textContent = amplitude;
+    gainNode.gain.setValueAtTime(amplitude / 100, audioCtx.currentTime);
 });
 
 playPauseButton.addEventListener("click", () => {
-  isPlaying = !isPlaying;
-  playPauseButton.textContent = isPlaying ? "Pausa" : "Reproducir";
-  if (isPlaying) {
-    if (isSoundEnabled) {
-      audioCtx.resume();
+    isPlaying = !isPlaying;
+    const icon = playPauseButton.querySelector('i');
+    if (isPlaying) {
+        if (isSoundEnabled) {
+            audioCtx.resume();
+        }
+        icon.className = 'fas fa-pause'; // Cambia el ícono a "pause"
+    } else {
+        audioCtx.suspend();
+        icon.className = 'fas fa-play'; // Cambia el ícono a "play"
     }
-  } else {
-    audioCtx.suspend();
-  }
 });
 
 resetButton.addEventListener("click", () => {
-  time = 0;
-  sourceX = 50;
-  draw();
+    time = 0;
+    sourceX = 50;
+    draw();
 });
 
 moveSourceButton.addEventListener("click", () => {
-  isMovingSource = !isMovingSource;
-  moveSourceButton.textContent = isMovingSource
-    ? "Parar Emisor"
-    : "Mover Emisor";
+    isMovingSource = !isMovingSource;
+    moveSourceButton.textContent = isMovingSource
+        ? "Parar Emisor"
+        : "Mover Emisor";
 });
 
 soundToggle.addEventListener("change", (e) => {
-  isSoundEnabled = e.target.checked;
-  if (isSoundEnabled && isPlaying) {
-    audioCtx.resume();
-  } else {
-    audioCtx.suspend();
-  }
+    isSoundEnabled = e.target.checked;
+    if (isSoundEnabled && isPlaying) {
+        audioCtx.resume();
+    } else {
+        audioCtx.suspend();
+    }
+});
+document.getElementById('soundToggle').addEventListener('change', function() {
+    const volumeIcon = document.querySelector('.card-body .col-auto i');
+    if (this.checked) {
+        volumeIcon.className = 'fas fa-volume-up fa-2x text-warning';
+    } else {
+        volumeIcon.className = 'fas fa-volume-off fa-2x text-gray-300';
+    }
 });
 
 canvas.addEventListener("mousedown", (e) => {
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
 
-  if (Math.hypot(mouseX - sourceX, mouseY - sourceY) < 10) {
-    dragTarget = "source";
-  } else if (Math.hypot(mouseX - receiverX, mouseY - receiverY) < 10) {
-    dragTarget = "receiver";
-  }
+    if (Math.hypot(mouseX - sourceX, mouseY - sourceY) < 10) {
+        dragTarget = "source";
+    } else if (Math.hypot(mouseX - receiverX, mouseY - receiverY) < 10) {
+        dragTarget = "receiver";
+    }
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (dragTarget) {
-    if (dragTarget === "source") {
-      sourceX = e.offsetX;
-      sourceY = e.offsetY;
-    } else if (dragTarget === "receiver") {
-      receiverX = e.offsetX;
-      receiverY = e.offsetY;
+    if (dragTarget) {
+        if (dragTarget === "source") {
+            sourceX = e.offsetX;
+            sourceY = e.offsetY;
+        } else if (dragTarget === "receiver") {
+            receiverX = e.offsetX;
+            receiverY = e.offsetY;
+        }
+        draw();
     }
-    draw();
-  }
 });
 
 canvas.addEventListener("mouseup", () => {
-  dragTarget = null;
+    dragTarget = null;
 });
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  ctx.arc(sourceX, sourceY, 10, 0, Math.PI * 2);
-  ctx.fill();
-
-  // Dibujar el emisor
-  ctx.drawImage(
-      sourceIcon,
-      sourceX - newWidth / 2,
-      sourceY - newHeight / 2,
-      newWidth,
-      newHeight
-  );
-
-  // Dibujar el círculo del receptor
-  ctx.beginPath();
-  ctx.arc(receiverX, receiverY, 10, 0, Math.PI * 2);
-  ctx.fill();
-
-  // Dibujar el receptor
-  ctx.drawImage(
-      receiverIcon,
-      receiverX - newWidth / 2,
-      receiverY - newHeight / 2,
-      newWidth,
-      newHeight
-  );
-
-  // Dibujar ondas
-  for (let i = 0; i < 10; i++) {
-    ctx.strokeStyle = `rgba(0, 0, 0, ${0.5 - i * 0.05})`;
-    ctx.lineWidth = 2;
     ctx.beginPath();
-    const radius = i * 100 + (time % 100);
-    ctx.arc(sourceX, sourceY, radius, 0, Math.PI * 2);
-    ctx.stroke();
-  }
+    ctx.arc(sourceX, sourceY, 10, 0, Math.PI * 2);
+    ctx.fill();
 
-  // Dibujar escala
-  ctx.fillStyle = "black";
-  ctx.font = "12px Arial";
-  for (let i = 0; i <= 5; i++) {
-    ctx.fillText(`${i}m`, i * 160, canvas.height - 10);
-  }
+    // Dibujar el emisor
+    ctx.drawImage(
+        sourceIcon,
+        sourceX - newWidth / 2,
+        sourceY - newHeight / 2,
+        newWidth,
+        newHeight
+    );
+
+    // Dibujar el círculo del receptor
+    ctx.beginPath();
+    ctx.arc(receiverX, receiverY, 10, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Dibujar el receptor
+    ctx.drawImage(
+        receiverIcon,
+        receiverX - newWidth / 2,
+        receiverY - newHeight / 2,
+        newWidth,
+        newHeight
+    );
+
+    // Dibujar ondas
+    for (let i = 0; i < 10; i++) {
+        ctx.strokeStyle = `rgba(0, 0, 0, ${0.5 - i * 0.05})`;
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        const radius = i * 100 + (time % 100);
+        ctx.arc(sourceX, sourceY, radius, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+
+    // Dibujar escala
+    ctx.fillStyle = "black";
+    ctx.font = "12px Arial";
+    for (let i = 0; i <= 5; i++) {
+        ctx.fillText(`${i}m`, i * 160, canvas.height - 10);
+    }
 }
+
 function updateVolumeBasedOnDistance() {
-  const distance = Math.sqrt(
-    Math.pow(sourceX - receiverX, 2) + Math.pow(sourceY - receiverY, 2)
-  );
-  const maxDistance = canvas.width;
-  const volume = 1 - Math.min(distance / maxDistance, 1);
-  gainNode.gain.value = volume;
+    const distance = Math.sqrt(
+        Math.pow(sourceX - receiverX, 2) + Math.pow(sourceY - receiverY, 2)
+    );
+    const maxDistance = canvas.width;
+    const volume = 1 - Math.min(distance / maxDistance, 1);
+    gainNode.gain.value = volume;
 }
 
 function animate() {
-  if (isPlaying) {
-    time += 1;
-    if (isMovingSource) {
-      sourceX += sourceSpeed;
-      if (sourceX > canvas.width) {
-        sourceX = 0;
-      }
-    }
+    if (isPlaying) {
+        time += 1;
+        if (isMovingSource) {
+            sourceX += sourceSpeed;
+            if (sourceX > canvas.width) {
+                sourceX = 0;
+            }
+        }
 
-    updateVolumeBasedOnDistance(); // Actualizar el volumen basado en la distancia
-    const distance = Math.hypot(sourceX - receiverX, sourceY - receiverY);
-    const attenuation = 1 / (1 + distance / 100);
-    gainNode.gain.value *= attenuation;
-  }
-  draw();
-  requestAnimationFrame(animate);
+        updateVolumeBasedOnDistance(); // Actualizar el volumen basado en la distancia
+        const distance = Math.hypot(sourceX - receiverX, sourceY - receiverY);
+        const attenuation = 1 / (1 + distance / 100);
+        gainNode.gain.value *= attenuation;
+    }
+    draw();
+    requestAnimationFrame(animate);
 }
 
 sourceIcon.onload = () => {
-  receiverIcon.onload = () => {
-    draw();
-  };
+    receiverIcon.onload = () => {
+        draw();
+    };
 };
 
 animate();
